3d6d72289022fb30fa9ddfb6595c3d2ccb15aca3
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-22 17:06:33 -0400

    Added sbteclipsify support.  Eclipse projects can now be created from within sbt by using the "eclipse" action.

--- project/build/MocapUtilsProject.scala
+++ project/build/MocapUtilsProject.scala
@@ -1,8 +1,11 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class MocapUtilsProject(info: ProjectInfo) extends DefaultProject(info) {
+class MocapUtilsProject(info: ProjectInfo) extends DefaultProject(info) 
+with Eclipsify {
 
-  val scalaToolsSnapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scalaToolsSnapshots = "Scala Tools Snapshots" at 
+    "http://scala-tools.org/repo-snapshots"
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0-SNAPSHOT"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "1.4.1"
