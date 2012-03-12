2443152292332f1c5c36e44dbdcd77973627e50f
Jonathan Merritt <merritt@unimelb.edu.au>
2012-03-12 16:02:07 -0400

    Upgraded to Scala 2.9.1-1. - Removed Scala-Tools Snapshots repository. - Added Sonatype repository. - Upgraded scalatest: 1.6.1 -> 1.7.1 - Upgraded scalala: 1.0.0.RC2-SNAPSHOT -> 1.0.0.RC2

--- project/MocapBuild.scala
+++ project/MocapBuild.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "com.github.mocaputils"
-  val buildScalaVersion = "2.9.1"
+  val buildScalaVersion = "2.9.1-1"
   val buildVersion      = "0.2-SNAPSHOT"
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
@@ -13,11 +13,13 @@ object BuildSettings {
 }
 
 object Resolvers {
-  val scalaToolsSnapshots = "Scala-Tools Snapshots" at
-    "http://scala-tools.org/repo-snapshots"
+  //val scalaToolsSnapshots = "Scala-Tools Snapshots" at
+  //  "http://scala-tools.org/repo-snapshots"
+  val sonatype = "Sonatype" at 
+  	"https://oss.sonatype.org/content/groups/public"
   val ondex = "Ondex" at
     "http://ondex.rothamsted.bbsrc.ac.uk/nexus/content/groups/public"
-  val allResolvers = Seq(scalaToolsSnapshots, ondex)
+  val allResolvers = Seq(/*scalaToolsSnapshots,*/ sonatype, ondex)
 }
 
 object Dependencies {
@@ -25,11 +27,11 @@ object Dependencies {
   val jFreeChart  = "org.jfree" % "jfreechart" % "1.0.14"
   val xmlGraphics = "org.apache.xmlgraphics" % "xmlgraphics-commons" % "1.4"
   val iText       = "com.lowagie" % "itext" % "2.1.5" intransitive()
-  val scalaz      = "org.scalaz" %% "scalaz-core" % "7.0-SNAPSHOT"
-  val scalaTest   = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
-  val scalaCheck  = "org.scala-tools.testing" %% "scalacheck" % "1.9" % 
+  val scalaz      = "org.scalaz" % "scalaz-core_2.9.1" % "7.0-SNAPSHOT"
+  val scalaTest   = "org.scalatest" %% "scalatest" % "1.7.1" % "test"
+  val scalaCheck  = "org.scala-tools.testing" % "scalacheck_2.9.0" % "1.9" % 
                       "test"
-  val scalala     = "org.scalala" %% "scalala" % "1.0.0.RC2-SNAPSHOT"
+  val scalala     = "org.scalala" % "scalala_2.9.1" % "1.0.0.RC2"
   val allDependencies = Seq(
     jCommon, jFreeChart, xmlGraphics, iText, scalaz, scalaTest, scalaCheck, 
     scalala
